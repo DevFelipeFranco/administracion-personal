68d9fae2799a481f539958380a42d2cf1bbb5fc9
felipefranco <devfelipefranco@gmail.com>
2021-01-02 12:17:15 -0800

    Construccion base del proyecto

---
+++ src/main/java/com/administracion/personal/AdministracionPersonalApplication.java
@@ -0,0 +1,13 @@
+package com.administracion.personal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdministracionPersonalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdministracionPersonalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/administracion/personal/AdministracionPersonalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.administracion.personal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdministracionPersonalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
